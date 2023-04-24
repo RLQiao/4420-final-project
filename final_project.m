07ae6b0904ef3c7609c110da4e62d2c1cae0a336
RLQiao <joannaq@uw.edu>
2023-04-24 13:55:03 -0400

    update new work

--- final_project.m
+++ final_project.m
@@ -16,25 +16,42 @@ for i = 1:8
     n_test(i) = data_struct.n_TEST;
     n_train(i) = data_struct.n_TRAIN;
     n_trials(i) = data_struct.nTrial;
-    % preprocess eegdata
-    preprocess(data_struct.X_EEG_TRAIN, sprintf('%s_train', filenames(i)));
-    preprocess(data_struct.X_EEG_TEST, sprintf('%s_test', filenames(i)));
-    % get and save train labels
+    for j = 1:length(Y_EEG_TRAIN)
+        if Y_EEG_TRAIN(j) ~= 1
+            separator = j;
+            break
+        end
+    end
+%     preprocess eegdata
+    preprocess(data_struct.X_EEG_TRAIN, sprintf('%s_train', filenames(i)), separator);
+    preprocess(data_struct.X_EEG_TEST, sprintf('%s_test', filenames(i)), separator);
+%     get and save train labels
     train_label = data_struct.Y_EEG_TRAIN;
-    label_path = sprintf('/Users/joannaq/Desktop/Spring 23/4420/final project/preprocessed_data/bandpass_filter_only/%s_label.mat', filenames(i)); 
+    label_path = sprintf('/Users/joannaq/Desktop/Spring 23/4420/final project/preprocessed_data/bandpass_60_ica/%s_label.mat', filenames(i)); 
     save(label_path, "train_label");
 end
 
-function [] = preprocess(data, filename)
+function [] = preprocess(data, filename, separator)
     % get eeglab
     eeglab redraw;
     % read eegdata
+    % adding channels to num_chan = 60 for subject 6
+    if contains(filename, "6")==1
+        data(58:60,:,:) = data(55:57,:,:);
+    end
     EEG = pop_importdata('dataformat','array','nbchan',60,'data', data,'srate',1000, ...
                          'chanlocs', '/Users/joannaq/Desktop/Spring 23/4420/final project/Signal_Processing_FC/standard60.loc');
     % bandpass filter
     EEG = pop_eegfiltnew(EEG, 'locutoff',0.5,'hicutoff',40); % 0.5-40 bandpass filter
+    % channel normalization each channel
+    EEG.data(1:separator-1,:,:) = normalize(EEG.data(1:separator-1,:,:),2); 
+    EEG.data(separator:end,:,:) = normalize(EEG.data(separator:end,:,:),2); 
+    % run ICA and reject articfacts
+    EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','on');
+    EEG = pop_iclabel(EEG, 'default');
+    EEG = pop_icflag(EEG, [NaN NaN;0.5 1;0.5 1;0.5 1;0.5 1;0.5 1;0.5 1]);
     % save eegdata back to .mat
-    save_path = sprintf('/Users/joannaq/Desktop/Spring 23/4420/final project/preprocessed_data/bandpass_filter_only/%s.mat',filename);
+    save_path = sprintf('/Users/joannaq/Desktop/Spring 23/4420/final project/preprocessed_data/bandpass_60_ica/%s.mat',filename);
     save(save_path, 'EEG', '-v7');
     eeglab redraw;
 end
